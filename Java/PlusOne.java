a0daebc1aa68a190130cc15876da28d673aac4bc
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2022-10-13 10:50:20 +0530

    Brute Force Approach

---
+++ Java/PlusOne.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        int i=n-1;
+        int a=0;
+        while(i>=0){
+            if(digits[i]!=9){
+                digits[i]+=1;
+                break;
+            }else{
+                if(i==0 && digits[i]==9){
+                    a++;
+                }
+                digits[i]=0;
+                i--;
+            }
+        }
+        if(a==0)
+        return digits;
+        else{
+            int arr[]=new int[n+1];
+            arr[0]=1;
+            return arr;
+        }
+    }
+}
